fe31be43a6558962e8da983121f64f38ee3950b3
yzhang <zhangyehua1996@gmail.com>
2025-04-26 20:39:29 -0500

    Initial Commit

---
+++ Football2/Football2App.swift
@@ -0,0 +1,17 @@
+//
+//  Football2App.swift
+//  Football2
+//
+//  Created by Yehua Zhang on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Football2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
